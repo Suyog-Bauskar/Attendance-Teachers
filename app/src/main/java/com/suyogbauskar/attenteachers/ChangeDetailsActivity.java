07307b601720588223f2de3cba0c8fa997ce8e62
Suyog-Bauskar <suyogbauskar2004@gmail.com>
2022-09-24 10:51:35 +0530

    Implemented bottom navigation bar

---
+++ app/src/main/java/com/suyogbauskar/attenteachers/ChangeDetailsActivity.java
@@ -0,0 +1,138 @@
+package com.suyogbauskar.attenteachers;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class ChangeDetailsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button firstnameBtn, lastnameBtn;
+    LinearLayout.LayoutParams params;
+    private FirebaseFirestore db;
+    boolean isProcessRemaining;
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_details);
+
+        Toolbar toolbar = findViewById(R.id.toolbarChangeDetails);
+        toolbar.setTitle("Change Details");
+        setSupportActionBar(toolbar);
+
+        db = FirebaseFirestore.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        firstnameBtn = findViewById(R.id.firstnameBtn);
+        lastnameBtn = findViewById(R.id.lastnameBtn);
+
+        firstnameBtn.setOnClickListener(this);
+        lastnameBtn.setOnClickListener(this);
+
+        params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins((int) (15 * getResources().getDisplayMetrics().density + 0.5f), (int) (15 * getResources().getDisplayMetrics().density + 0.5f), (int) (15 * getResources().getDisplayMetrics().density + 0.5f), 0);
+    }
+
+    @Override
+    public void onClick(View v) {
+        AlertDialog.Builder alert;
+        LinearLayout layout;
+        isProcessRemaining = false;
+
+        switch (v.getId()) {
+            case R.id.firstnameBtn:
+
+                alert = new AlertDialog.Builder(ChangeDetailsActivity.this);
+                alert.setTitle("Change Firstname");
+
+                layout = new LinearLayout(getApplicationContext());
+                layout.setOrientation(LinearLayout.VERTICAL);
+
+                final EditText newFirstname = new EditText(getApplicationContext());
+                newFirstname.setHint("New firstname");
+                newFirstname.setLayoutParams(params);
+                layout.addView(newFirstname);
+
+                alert.setView(layout);
+
+                alert.setPositiveButton("Save", (dialog, whichButton) -> {
+                    String firstname = newFirstname.getText().toString();
+
+                    if (!firstname.trim().isEmpty() && firstname.chars().allMatch(Character::isLetter)) {
+                        firstname = firstname.trim();
+                        db.collection("data").document(user.getUid()).update("firstname", firstname);
+                        Toast.makeText(getApplicationContext(), "Firstname Saved", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Invalid firstname", Toast.LENGTH_LONG).show();
+                        isProcessRemaining = true;
+                    }
+                    if (isProcessRemaining) {
+                        isProcessRemaining = false;
+                        firstnameBtn.performClick();
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", (dialog, whichButton) -> dialog.dismiss());
+
+                alert.show();
+
+                break;
+
+            case R.id.lastnameBtn:
+
+                alert = new AlertDialog.Builder(ChangeDetailsActivity.this);
+                alert.setTitle("Change Lastname");
+
+                layout = new LinearLayout(getApplicationContext());
+                layout.setOrientation(LinearLayout.VERTICAL);
+
+                final EditText newLastname = new EditText(getApplicationContext());
+                newLastname.setHint("New lastname");
+                newLastname.setLayoutParams(params);
+                layout.addView(newLastname);
+
+                alert.setView(layout);
+
+                alert.setPositiveButton("Save", (dialog, whichButton) -> {
+                    String lastname = newLastname.getText().toString();
+                    if (!lastname.trim().isEmpty() && lastname.chars().allMatch(Character::isLetter)) {
+                        lastname = lastname.trim();
+                        db.collection("data").document(user.getUid()).update("lastname", lastname);
+                        Toast.makeText(getApplicationContext(), "Lastname Saved", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Invalid lastname", Toast.LENGTH_LONG).show();
+                        isProcessRemaining = true;
+                    }
+                    if (isProcessRemaining) {
+                        isProcessRemaining = false;
+                        lastnameBtn.performClick();
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", (dialog, whichButton) -> dialog.dismiss());
+
+                alert.show();
+
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+    }
+}
--- app/src/main/java/com/suyogbauskar/attenteachers/HomeActivity.java
+++ app/src/main/java/com/suyogbauskar/attenteachers/HomeActivity.java
@@ -1,28 +1,62 @@
 package com.suyogbauskar.attenteachers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button signOutBtn;
+    private BottomNavigationView bottomNav;
+    public static int theme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        signOutBtn = findViewById(R.id.signoutBtn);
-        signOutBtn.setOnClickListener(v -> {
-            FirebaseAuth.getInstance().signOut();
-            startActivity(new Intent(HomeActivity.this, MainActivity.class));
+        Toolbar toolbar = findViewById(R.id.toolbarHome);
+        toolbar.setTitle("Attendance");
+        setSupportActionBar(toolbar);
+
+        bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    return true;
+
+                case R.id.profile:
+                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+
+                case R.id.settings:
+                    startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                    overridePendingTransition(0, 0);
+                    return true;
+            }
+            return false;
         });
+        bottomNav.setSelectedItemId(R.id.home);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("themePref", MODE_PRIVATE);
+        theme = sharedPreferences.getInt("theme", 0);
+
+        if (theme == 0) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        } else if (theme == 1) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else if (theme == 2) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
     }
 
     @Override
