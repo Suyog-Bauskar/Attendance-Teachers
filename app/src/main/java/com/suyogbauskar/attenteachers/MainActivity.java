62e680ace128d5fe0ce40d9f3f9b8cb99a6f897f
Suyog-Bauskar <suyogbauskar2004@gmail.com>
2022-09-24 09:24:32 +0530

    Completed login UI

---
+++ app/src/main/java/com/suyogbauskar/attenteachers/MainActivity.java
@@ -0,0 +1,28 @@
+package com.suyogbauskar.attenteachers;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db;
+    private FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
